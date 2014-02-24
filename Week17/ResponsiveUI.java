8ee9368844c95c4a32049cd7f5019e987121b095
unknown <mjones28@cspc061.dcs.bbk.ac.uk>
2014-02-24 21:04:37 +0000

    Week 19 networking

--- Week17/ResponsiveUI.java
+++ Week17/ResponsiveUI.java
@@ -1,13 +1,35 @@
+import java.util.ArrayList;
+
 public class ResponsiveUI implements Runnable 
 {
 	private int sleeper;
-		
+	private static int counter;
+	private static ArrayList<Integer> complete = new ArrayList<Integer>();
+	
+	private int taskNo = 0;
+	
 	public ResponsiveUI(int timer)
 	{
 		this.sleeper = timer;	
+		taskNoInit(counter);
+		counter = counter + 1;
 	}	
 	
-	
+		public void isComplete()
+		{
+			if (!complete.isEmpty())
+			{
+				System.out.print("Task Complete: ");
+				for (int i = 0; i < complete.size(); i++)
+				{
+					System.out.print( complete.get(i) + ",");
+				}
+				System.out.println();
+				//clears the arraylist
+				complete.clear();
+			}
+			
+		}
 
 	
 		public static void main(String args[]) 
@@ -17,20 +39,26 @@ public class ResponsiveUI implements Runnable
 			{	
 				System.out.print("Enter the duration (in ms) of task " + i + ":");
 				String str = System.console().readLine();
+	
 				int timer = Integer.parseInt(str);
 				ResponsiveUI response = new ResponsiveUI(timer);
 				
 				Thread t = new Thread(response);
 				t.start();
+				response.isComplete();
 			}
+			
+			
 		}
 
 
 	public void run()
 	{
 	try {
-		Thread.sleep(this.sleeper);
-		System.out.println("finished");	
+			Thread.sleep(this.sleeper);
+			//System.out.println("\n ***DEBUG*** Task: " + getTaskNo() + " finished");	
+			complete.add(getTaskNo()); 
+			
 		} 
 		catch (InterruptedException ex)
 		{
@@ -39,6 +67,14 @@ public class ResponsiveUI implements Runnable
 	
 	}
 
+	
+	public void taskNoInit(int counter)
+	{	
+		taskNo = counter; 
+	}
 
-
+	public int getTaskNo()
+	{
+		return taskNo;
+	}
 }
---
+++ github.bat
@@ -0,0 +1,5 @@
+set PATH=%PATH%;C:\Program Files (x86)\Git\bin
+set PATH=%PATH%;C:\Program Files\Java\jdk1.7.0_25\bin
+set CLASSPATH=%CLASSPATH%;H:\JUNIT_HOME\junit-4.11.jar;
+set CLASSPATH=%CLASSPATH%;H:\JUNIT_HOME\hamcrest-core-1.3.jar;
+start
